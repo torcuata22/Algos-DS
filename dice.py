396e050a342ba40d2ddc886764f58655e3c1d91e
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-05-28 11:44:34 -0400

    dice game

--- dice.py
+++ dice.py
@@ -3,15 +3,18 @@ import random
 high_score = 0
 
 
+
 def dice_game():
     global high_score
+    roll_dice = "yes"
     print("Your current High Score:", high_score)
     print("1) Roll the dice")
     print("2) Leave the game")
     x = input("Enter your choice")
     print("\n")
 
-    while True:
+
+    while roll_dice =="yes" or roll_dice  == "y":
         if x == "1":
             roll_dice1 = random.randint(1, 6)
             roll_dice2 = random.randint(1, 6)
@@ -21,17 +24,20 @@ def dice_game():
             print("You have rolled a total of:", total_roll)
 
             if high_score < total_roll:
-                high_score = total_roll
+                high_score = total_roll + high_score
                 print("Congratulations! You have a new high score:", total_roll)
 
             if total_roll < high_score:
+                high_score = total_roll + high_score
                 print("Your high score is", high_score)
-
+                
+            print("select yes or no")
+            roll_dice = input("Roll the dice again?")
         else:
             print("Goodbye!")
-            break
+    
 
-        break
+
 
 
 dice_game()
