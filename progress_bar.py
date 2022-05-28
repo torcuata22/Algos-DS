889e13108c5aa3b6bbb4e9141b25c3e63f299302
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-05-28 11:03:26 -0400

    progress bar

---
+++ progress_bar.py
@@ -0,0 +1,12 @@
+
+for i in range(0, 101, 5): #range function: range(start, end --non inclusive--, increments)
+    print(i)
+    if i == 25:
+        print("One quarter of the way")
+    if i == 50:
+        print ("You are half way there")  
+    if i == 75:
+        print("Three quarters of the way")
+    if i ==100:
+        print ("Fully loaded")
+    
