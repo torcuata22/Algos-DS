f80e65c44a04567978430ebacd46c77375a45c89
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-05-28 13:18:02 -0400

    snow

--- snow.py
+++ snow.py
@@ -7,9 +7,9 @@ def print_total_inches(snow_fall):
         total_inches += inches
         print("Total snowfall in inches: ", total_inches)
 
-
 print_total_inches(snow_fall)
 
+#adding records for Thursday
 user_input = input("How many inches fell on Thursday?")
 snow_fall["Thursday"] = int(user_input)
 print_total_inches(snow_fall)
