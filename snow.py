dcbe9167bba15195e7bfb1541d8349be31ec8dda
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-05-28 13:14:28 -0400

    dictionary

---
+++ snow.py
@@ -0,0 +1,15 @@
+#using dictionry to keep record of snowfall
+snow_fall = {"Monday": 2, "Tuesday": 4, "Wednesday": 5}
+
+def print_total_inches(snow_fall):
+    total_inches = 0
+    for inches in snow_fall.values():
+        total_inches += inches
+        print("Total snowfall in inches: ", total_inches)
+
+
+print_total_inches(snow_fall)
+
+user_input = input("How many inches fell on Thursday?")
+snow_fall["Thursday"] = int(user_input)
+print_total_inches(snow_fall)
