579afdaeb924eb9ff8b40994d2b842401d359620
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-06-18 17:00:02 -0400

    stairs corrected

--- Hackerrank/staircase.py
+++ Hackerrank/staircase.py
@@ -1,13 +1,15 @@
-def staircase(n):
-    for i in range (0,n):
-        s = n-i-1
-        hash=i+1
-        print(s*" ",hash* "#")
+#Works, but Hackerrank wouldn't accept it:
+# def staircase(n):
+#     for i in range (0,n): Maybe it's because of the ZERO, it should be 1
+#         s = n-i-1
+#         hash=i+1
+#         print(s*" ",hash* "#")
         
     
-       
-        
-        
+#It accepts this:      
+def staircase(n):
+    for stairs in range(1, n + 1):
+        print(' ' * (n - stairs) + '#' * stairs)
         
         
 staircase (10)
