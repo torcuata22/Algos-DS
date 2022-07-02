79000bae8253a5dc456c9b2d96ad4ce926688c19
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-07-02 16:49:55 -0400

    reverse a linked list explanation

--- Hackerrank/reverseLinkedList.py
+++ Hackerrank/reverseLinkedList.py
@@ -1,11 +1,12 @@
 def reverse(head):
+#start by initializing 3 pointers:
     prv = None
     cur = head
     next = head.next
-    while cur != None:
+    while cur != None: #will run as long as the current node is not null
         next = cur.next
-        cur.next = prv
-        prv = cur
+        cur.next = prv #changes the direction of the pointer
+        prv = cur #these last two shift the nodes
         cur = next
     head = prv
     return head
