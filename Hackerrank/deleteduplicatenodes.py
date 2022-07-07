94e3351d7891ce1040d5ae73c416c815c0ec22d5
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-07-07 15:39:16 -0400

    completed function

--- Hackerrank/deleteduplicatenodes.py
+++ Hackerrank/deleteduplicatenodes.py
@@ -1,2 +1,14 @@
 #to delete repeating values in adyacent nodes: I need to create a temp variable to perform the operations and compare values
 #to delete the node, I just skip it
+#to compare values store in each node I use head.data
+
+def removeDuplicates(head):
+    temp = head
+    while temp.next !=None:
+        if temp.data == temp.next.data:
+            temp.next = temp.next.next
+            
+        else: 
+            temp = temp.next
+            
+    return head
