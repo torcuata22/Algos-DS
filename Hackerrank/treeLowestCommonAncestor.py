fb3aa0252e244529ab2d1e5d9ab9e80eab83dbe6
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-07-11 23:11:14 -0400

    lower common ancestor tree

---
+++ Hackerrank/treeLowestCommonAncestor.py
@@ -0,0 +1,13 @@
+#v1 always < than v2, if not, I need to swao them (to maintain the condition), and v1 is always different than v2
+#if both values are smaller than root.info, go to the left of the root
+#if both are greater, go to the right
+
+def lca(root, v1, v2):
+    if v1>v2:
+        v1, v2 = v2, v1
+    if v1 < root.info and v2 < root.info:
+        root=root.left
+    elif v1 > root.info and v2 > root.info:
+        root = root.right
+    else:
+        return root
