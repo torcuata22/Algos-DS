35bf115d3a9a4dbee7a4c46aa113b1c005c57ee6
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-07-11 22:31:15 -0400

    tree level order

---
+++ Hackerrank/treeLevelOrder.py
@@ -0,0 +1,15 @@
+#print the tree level by level insted of noded. For this, use queue, this way we cna explore all levels
+
+from collections import deque
+def levelOrder(root):
+    q = deque([root])
+    #as long as there are elements in q
+    while len(q):
+        root = q.popleft() #remove the first element of the queue
+        print(root, end=" ")
+        if root.left:#append left element if there is one
+            q.append(root.left)
+        if root.right: #append right element if there is one
+            q.append(root.right)
+            
+        #this will repeqt until len(q)=0
