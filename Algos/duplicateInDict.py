30fb4d2b3b2cf208f5270293ac84c4a2f7b1fd9b
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-07-14 12:25:36 -0400

    remove duplicates from list of dictionaries

--- Algos/duplicateInDict.py
+++ Algos/duplicateInDict.py
@@ -1,2 +1,7 @@
 #To remove duplicate values from a list of dictionaries, we can use a loop
 items = [{'name':'Nikki'},{'name':'Kate'}, {'name':'James'}, {'name':'Nikki'}, {'name':'Kate'}, {'name':'Mike'}]
+unique = []
+for item in items:
+    if item not in unique:
+        unique.apend(item)
+    print (unique)
