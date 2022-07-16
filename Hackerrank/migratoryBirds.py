7ce5b7dab1a3cf258dbf188f26d4b470e1e7145d
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-07-16 17:10:52 -0400

    migratory birds

--- Hackerrank/migratoryBirds.py
+++ Hackerrank/migratoryBirds.py
@@ -2,7 +2,7 @@
 #If more than one id, return the lowest id value
 
 def migratoryBirds(arr):
-    l=[0] * len(arr)
+    l=[0] * len(arr) #creates a list with len(arr) elements
     for i in range (len(arr)):
-        l[arr[i]] +=1
-    return l.index(max(l))
+        l[arr[i]] +=1 #increment count for particular bird in index i
+    return l.index(max(l)) #max(l) calculates max count, index returns index(type) of bird
