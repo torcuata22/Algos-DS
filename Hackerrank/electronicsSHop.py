7f06ab8702105c9bcbdaeb7746284c7b33483fcc
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-07-18 22:36:15 -0400

    electronics shop

--- Hackerrank/electronicsSHop.py
+++ Hackerrank/electronicsSHop.py
@@ -1,6 +1,6 @@
 #determine most expensive keyboard and USB drive that you can buy with budget b
 #keyboards and drives are list of prices
-#need to add all keyboards[i] with drives[i] and verify which combinations are <=b
+#need to add all keyboards[i] with drives[j] and verify which combinations are <=b
 
 def getMoneySpent(keyboards, drives, b):
     result = -1
