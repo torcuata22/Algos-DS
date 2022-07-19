c385a81de7f3f41802bb3e1c2086f3598fdbc6bc
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-07-19 16:04:15 -0400

    magic square

--- Hackerrank/formingMagicSquare.py
+++ Hackerrank/formingMagicSquare.py
@@ -1,2 +1,8 @@
 #Form a magic square: n*n matrix of ints from 1 to n**2 where sum rows, sum cols, sum diagonals = constant
 #(the magic constant)
+
+def formingMagicSquare(s):
+    magic =  [[8, 1, 6, 3, 5, 7, 4, 9, 2], [6, 1, 8, 7, 5, 3, 2, 9, 4], [4, 3, 8, 9, 5, 1, 2, 7, 6], 
+              [2, 7, 6, 9, 5, 1, 4, 3, 8],  [2, 9, 4, 7, 5, 3, 6, 1, 8], [4, 9, 2, 3, 5, 7, 8, 1, 6], 
+              [6, 7, 2, 1, 5, 9, 8, 3, 4], [8, 3, 4, 1, 5, 9, 6, 7, 2]]
+    pass
