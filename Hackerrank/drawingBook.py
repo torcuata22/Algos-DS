aa95c20e82080526bfffe5eb1cf4c92dff996980
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-07-18 22:06:44 -0400

    drawing book

--- Hackerrank/drawingBook.py
+++ Hackerrank/drawingBook.py
@@ -1,3 +1,10 @@
 #n=number of pages, p=page we want to go to.COunt number of page turns to get to p
 #starting on page 1 and starting on last page (page n), return minimum of the two
+#number of turned pages = n/2
 def pageCount(n,p):
+    front = p//2
+    if n%2 == 1: #if it's odd
+        back = (n-p)//2
+    else: #if it's even
+        back = (n-p+1)//2
+    return min(front, back)
