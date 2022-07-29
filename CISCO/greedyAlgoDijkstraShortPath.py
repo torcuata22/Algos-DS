d2dae8e515bd8c4e1ae462b350ee6a397f13ca9e
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-07-29 16:50:13 -0400

    algo

---
+++ CISCO/greedyAlgoDijkstraShortPath.py
@@ -0,0 +1,5 @@
+#given network of n nodes, labeled from 1 to n, also given times, a list of travel times as directed edges:
+#times[i] = (ui,vi,wi), where ui is the source node and wi = time it takes signal to travel source to target
+#we will send signal from node k. Return time it takes for all the n nodes to receive the signal
+#If it is impossible for all the n nodes to receive signal, return -1
+
