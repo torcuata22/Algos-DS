4b1ad46a636c119385b54137eb07925f5fe15cb7
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-07-29 16:29:08 -0400

    debugged

--- CISCO/insertionSort.py
+++ CISCO/insertionSort.py
@@ -1,17 +1,19 @@
 #Insertion Sort Algorithm: 
 #need to move elements or array ascending order, so check key = arr[i], if element in array is > key, it
-#moves one position ahead of current position
+#moves one position ahead of current position, running time: o(n**2)
 
 def insertionSort(arr):
-    for i in range (1, len(arr)):
+    for i in range (1, len(arr)): #goes from index 1 to index 5
         key = arr[i]
-        print (key)
-        j=i-1
-        while j >= 0 and key < arr[j]:
+        j=i-1 #left neighbor (we compare to left neighbor)
+        while j >= 0 and key < arr[j]: #key is element to the right of arr[j], so, as long as right element is smaller than left element
             arr[j+1] = arr[j]
-            j-=1
+            j-=1 #to go further to the left after swap
         arr[j+1]=key
-    print (arr)
+   
             
 a=[12,11,13,5,6]
 insertionSort(a)
+print (a)
+
+
