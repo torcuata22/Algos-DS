4e34e607d999e2ff8536da0be85b8f63c570b14b
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-08-03 16:25:41 -0400

    matrices

---
+++ CISCO/rotateImage.py
@@ -0,0 +1,16 @@
+#see notes (problem 20)
+
+def rotateMatrix(self, matrix:list[list[int]])->None:
+    l,r = 0, len(matrix)-1
+    
+    while l < r:
+        for i in range(r-l):
+            top, bottom = l, r
+            topLeft = matrix[top][l+i]
+            matrix[top][l-i] = matrix[bottom-i][l]
+            matrix[bottom - i][l] = matrix[bottom][r-i]
+            matrix[bottom][r-i] = matrix[top+i][r]
+            matrix[top+i][r] = topLeft
+        r-=1
+        l+=1
+        
