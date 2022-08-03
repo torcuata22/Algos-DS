0f0861b8dfdde0839f58d3cee3be7f95dde81bea
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-08-02 22:05:05 -0400

    permutation numbers

---
+++ CISCO/permutationArrNumbers.py
@@ -0,0 +1,20 @@
+#given array of numbers, find all possible permutations (all numbers are distinct ints)
+#we will use recursion for this
+#base case: if len(nums) == 1, in which case, we return list of lists with
+#one element by making a copy inside list
+
+def permute(self, nums:list[int])->list[list[int]]:
+    result = []
+    if len(nums) == 1:
+        return [nums.copy()]
+    
+    for i in range(len(nums)):
+        n = nums.pop(0)
+        perms = self.permute(nums)
+
+        for perm in perms:
+            perm.append(n)
+        result.extend(perms)
+        nums.append(n)
+    return result
+    
