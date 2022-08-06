dda8b1228565bd15f57e6f3c7369d5394beca3e7
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-08-05 21:36:54 -0400

    added degree

--- Leetcode/degreeArray.py
+++ Leetcode/degreeArray.py
@@ -10,7 +10,26 @@ def arrayDegree(nums):
     print (count)
     
 numbers = [1,2,3,2,2,4,5,7,6,2,]
-arrayDegree(numbers)
+#arrayDegree(numbers)
             
         
+#For degree and length of shortest subarray containing repeated numbers:
 
+
+def findShortestSubArray( nums):
+    left, right, count = {}, {}, {} #initialize three empty dicts
+    for i, x in enumerate(nums): #enumerate generates object with a counter, i=index and x=value
+        if x not in left: 
+            left[x] = i
+        right[x] = i
+        count[x] = count.get(x, 0) + 1 #get() is a dictionary method takes key name (mandatory) and value(optional)
+
+    ans = len(nums)
+    degree = max(count.values())
+    for x in count:
+        if count[x] == degree:
+            ans = min(ans, right[x] - left[x] + 1)
+
+    print(ans)
+
+findShortestSubArray(numbers)
