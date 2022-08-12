180fded0928514b67600f080c15a202aa7b91331
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-08-12 13:07:26 -0400

    finished algo

--- Leetcode/medianSortedArrays.py
+++ Leetcode/medianSortedArrays.py
@@ -9,5 +9,21 @@ def findMedianSortedArrays(nums1, nums2):
         A,B = B,A
     l,r = 0, len(A)-1
     while True:
+        i = (l + r) // 2
+        j = half - i - 2
+        Aleft = A[i] if i >=0 else float("-infinity")
+        Aright = A[i+1] if (i+1) < len(A) else ("infinity")
+        Bleft = B[j] if j >= 0 else float("-infinity")
+        Bright = B[j+1] if (j+1) < len(B) else ("-infinity")
         
+        if Aleft <= Bright and Bleft <= Aright:
+            if total%2:
+                return min(Aright, Bright)
+            return (max(Aleft, Bleft) + min(Aright, Bright))/2
+        
+        elif Aleft > Bright:
+                r = i-1
+        else:
+            l=i+1
+            
     
