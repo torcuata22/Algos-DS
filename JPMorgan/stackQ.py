833785af5dbdd7d39d0b5e92fe76ac9a83e0f756
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-08-15 14:14:23 -0400

    algos

--- JPMorgan/stackQ.py
+++ JPMorgan/stackQ.py
@@ -9,5 +9,38 @@ class Stack:
     def push(self, data):
         s= len(self.q)
         self.q.append(data) #push the current element
-        for i in range (s): #pop all previous elements and put them after current element
+        for i in range (s): #pop all previous elements and put them after current element: first we pop, then we append
+            self.q.append(self.q.popleft())
+    
+    #we need to remove the top element because queues are FIFO and stacks are LIFO
+    def pop(self):
+        if not self.q:
+            print("No elements")
+        else:
+            self.q.popleft()
             
+    #finally we need to return the top of the stack
+    def top(self):
+        if not self.q:
+            return
+        return self.q[0]
+    
+    #driver code:
+    if __name__ == '__main__':
+        st = Stack()
+    st.push(40)
+    st.push(50)
+    st.push(70)
+    print(st.top())
+    st.pop()
+    print(st.top())
+    st.pop()
+    print(st.top())
+    st.push(80)
+    st.push(90)
+    st.push(100)
+    print(st.top())
+    st.pop()
+    print(st.top())
+    st.pop()
+    print(st.top())
